a654bdd33f25b40112d818c5e4a93a97f36fb22e
IshaGupta2-lab <ishaashi254@gmail.com>
2026-03-14 14:37:42 +0530

    Add files via upload

---
+++ bestTimeToBuyAndSell4.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static int solve(int index,int buy,int[] prices,int limit,int[][][] dp){
+        if(index==prices.length) return 0;
+        if(limit==0) return 0;
+        if(dp[index][buy][limit]!=-1) return dp[index][buy][limit];
+        int profit=0;
+        if(buy==1){
+            int buyKaro=-prices[index]+solve(index+1,0,prices,limit,dp);
+            int skipKaro=0+solve(index+1,1,prices,limit,dp);
+            profit=Math.max(buyKaro,skipKaro);
+        }
+        else{
+            int sellKaro=prices[index]+solve(index+1,1,prices,limit-1,dp);
+            int skipKaro=0+solve(index+1,0,prices,limit,dp);
+            profit=Math.max(sellKaro,skipKaro);
+        }
+        return dp[index][buy][limit]=profit;
+    }
+    public int maxProfit(int k, int[] prices) {
+        int n=prices.length;
+        int[][][] dp=new int[n][2][k+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                Arrays.fill(dp[i][j],-1);
+            }
+        }
+        return solve(0,1,prices,k,dp);
+    }
+}
