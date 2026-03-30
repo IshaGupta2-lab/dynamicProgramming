0be1c8688fb875915864fda99f88330f7409d858
IshaGupta2-lab <ishaashi254@gmail.com>
2026-03-30 18:30:20 +0530

    Add files via upload

---
+++ kefaAndFirstSteps.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class kefaAndFirstSteps {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max=1;
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(arr[i]>=arr[i-1]){
+                count++;
+            }else{
+                count=1;
+            }
+            max=Math.max(max, count);
+        }
+        System.out.println(max);
+    }
+}
